8c99e5311763f97c743638a70de4748d8eea9bcf
merima98 <merimaceranic98@gmail.com>
2022-04-13 11:47:04 +0200

    Added fireEvent.click (dark/light mode)

--- src/__tests__/button.spec.js
+++ src/__tests__/button.spec.js
@@ -1,12 +1,24 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Button from "../components/Button";
 
 describe("Should test Button component.", function () {
-  beforeEach(() => {
-    //Before each test, we will render Button component.
+  it("Should render Button component.", function () {
     render(<Button />);
   });
   it("Should have button tag.", function () {
+    render(<Button />);
     expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+  it("Should have function.", function () {
+    render(<Button />);
+    expect(screen.getByText("Dark/Light mode")).toBeInTheDocument();
+  });
+  it("Should change dark mode.", function () {
+    const darkMode = false;
+    const setIsDarkMode = jest.fn(() => !darkMode);
+    render(<Button darkMode={darkMode} setIsDarkMode={setIsDarkMode} />);
+    expect(screen.getByTestId("main-header")).toBeTruthy();
+    fireEvent.click(screen.queryByTestId("dark-mode-action"));
+    expect(setIsDarkMode).toBeCalledWith(true);
   });
 });
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <button>Button</button>;
+const Button = ({ darkMode, setIsDarkMode }) => {
+  return (
+    <div data-testid="main-header">
+      <button
+        data-testid="dark-mode-action"
+        onClick={() => setIsDarkMode(!darkMode)}
+      >
+        Dark/Light mode
+      </button>
+    </div>
+  );
 };
 
 export default Button;
