373e3bee2c2b295d417eb79c2784910739fe1334
merima98 <merimaceranic98@gmail.com>
2022-04-13 14:36:13 +0200

    Added checkbox component and test.

--- src/__tests__/checkbox.spec.js
+++ src/__tests__/checkbox.spec.js
@@ -1,9 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import Checkbox from "../components/Checkbox";
 
-describe("Should test checkbox component. ", function () {
+fdescribe("Should test checkbox component. ", function () {
   it("Should render checkbox component.", function () {
-    render(<Checkbox />);
+    const deleteButton = "Delete";
+    render(<Checkbox buttonText={deleteButton} />);
     expect(screen.getByTestId("checkbox-action")).toBeTruthy();
+  });
+  it("Should have checkbox-clas.", function () {
+    const deleteButton = "Delete";
+    render(<Checkbox buttonText={deleteButton} />);
+    expect(screen.getByTestId("checkbox-class")).toBeTruthy();
+  });
+  it("Should have button tag.", function () {
+    const deleteButton = "Delete";
+    render(<Checkbox buttonText={deleteButton} />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+  it("Should have Delete button text.", function () {
+    const deleteButton = "Delete";
+    render(<Checkbox buttonText={deleteButton} />);
+    expect(screen.getByTestId("data-testid-button-text")).toBeTruthy();
+    expect(screen.getByTestId("data-testid-button-text")).toHaveTextContent(
+      "Delete"
+    );
   });
 });
--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,7 +1,9 @@
-const Checkbox = () => {
+const Checkbox = ({ buttonText }) => {
   return (
     <div className="checkbox-holder" data-testid="checkbox-action">
-      Chcekbox component
+      <nav className="checkbox-class" data-testid="checkbox-class">
+        <button data-testid="data-testid-button-text">{buttonText}</button>
+      </nav>
     </div>
   );
 };
